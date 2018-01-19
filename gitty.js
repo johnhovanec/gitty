e353ee4c1041c5226b8987383a78b290d0116d45
jhovan1 <jhovan1@users.noreply.github.com>
2018-01-19 16:07:55 -0500

    Add images for users

--- gitty.js
+++ gitty.js
@@ -2,12 +2,14 @@ function getUsername(formName) {
   console.log(gitty.setName(formName));
   console.log(">> ", gitty.getName());
   console.log(gitty.getGitHubAccount());
+  
   return false;
 }
 
 // gitty module with Revealing Module pattern
 var gitty = (function() {
   var userName = "";
+  var userAvatar = "";
   var rootUrl = "https://api.github.com";
   
   var getUserName = function() {
@@ -25,8 +27,18 @@ var gitty = (function() {
         xhr.onload = function() {
             if (xhr.status === 200) {
                 console.log('User\'s name is ' + xhr.responseText);
-                obj = JSON.parse(xhr.responseText);
-                console.log(obj.name);
+                var obj = JSON.parse(xhr.responseText);
+                userAvatar = obj.avatar_url;
+              
+                // extract this to a function
+                var image = document.createElement("img");
+                var imageParent = document.getElementById("imageDiv");
+                image.id = "avatar";
+                image.width = 175;
+                image.src = userAvatar;
+                //element.src = "https://avatars0.githubusercontent.com/u/65382?v=4">";
+                imageParent.appendChild(image);
+                console.log(userAvatar);
             }
             else {
                 console.log('Request failed.  Returned status of ' + xhr.status);
@@ -35,10 +47,15 @@ var gitty = (function() {
     xhr.send();
   };
   
+  var getPicture = function() {
+    return userAvatar;
+  };
+  
   return {
     setName: setUserName,
     getName: getUserName,
-    getGitHubAccount: getGitHubAccount
+    getGitHubAccount: getGitHubAccount,
+    getPicture: getPicture
   }
 })();
 
